bb6e0cfbdb90ea6b31dde10da9e3bfe7dc764d0d
gengjiawen <technicalcute@gmail.com>
2016-07-30 16:33:22 +0800

    reformat code

--- render.js
+++ render.js
@@ -29,17 +29,17 @@ print("hello world")
   render: function() {
     return (
       <div className="AsciidoctorEditor">
-      <h3>Input</h3>
-      <textarea
-    onChange={this.handleChange}
-    ref="textarea"
-    defaultValue={this.state.value} />
-    <h3>Output</h3>
-    <div
-    className="content"
-    dangerouslySetInnerHTML={this.rawMarkup()}
-    />
-    </div>
+        <h3>Input</h3>
+        <textarea
+          onChange={this.handleChange}
+          ref="textarea"
+          defaultValue={this.state.value}/>
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.rawMarkup()}
+        />
+      </div>
     );
   }
 });
